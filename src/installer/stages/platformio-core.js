5c5aa190c4bdaa28e3c7e718e446ffb6f994d6c8
Ivan Kravets <me@ikravets.com>
2018-11-17 16:39:18 +0200

    Update Virtualenv to 16.1.0

--- src/installer/stages/platformio-core.js
+++ src/installer/stages/platformio-core.js
@@ -22,9 +22,9 @@ export default class PlatformIOCoreStage extends BaseStage {
   static UPGRADE_PIOCORE_TIMEOUT = 86400 * 7 * 1000; // 7 days
 
   static pythonVersion = '2.7.13';
-  static virtualenvUrl = 'https://pypi.python.org/packages/source/v/virtualenv/virtualenv-16.0.0.tar.gz';
-  static pioCoreDevelopUrl = 'https://github.com/platformio/platformio/archive/develop.zip';
   static pipUrl = 'https://files.pythonhosted.org/packages/69/81/52b68d0a4de760a2f1979b0931ba7889202f302072cc7a0d614211bc7579/pip-18.0.tar.gz';
+  static virtualenvUrl = 'https://files.pythonhosted.org/packages/4e/8b/75469c270ac544265f0020aa7c4ea925c5284b23e445cf3aa8b99f662690/virtualenv-16.1.0.tar.gz';
+  static pioCoreDevelopUrl = 'https://github.com/platformio/platformio/archive/develop.zip';
 
   constructor() {
     super(...arguments);
