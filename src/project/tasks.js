88db56f48bb2fc03efb4b5db2c1e9cbabd39e8b0
Ivan Kravets <me@ikravets.com>
2020-06-09 21:44:07 +0300

    Refactor project tasks and add support dev-for platform targets

--- src/project/tasks.js
+++ src/project/tasks.js
@@ -6,11 +6,11 @@
  * the root directory of this source tree.
  */
 
-import ProjectConfig from './config';
-import path from 'path';
+import * as core from '../core';
+import * as proc from '../proc';
 
 export class ProjectTasks {
-  static baseTasks = [
+  static genericTasks = [
     {
       name: 'Build',
       args: ['run'],
@@ -29,66 +29,11 @@ export class ProjectTasks {
     {
       name: 'Upload and Monitor',
       args: ['run', '--target', 'upload', '--target', 'monitor'],
-      multienv: true,
-      filter: data => !data.platform.includes('riscv_gap')
-    },
-    {
-      name: 'Upload using Programmer',
-      args: ['run', '--target', 'program'],
-      multienv: true,
-      filter: data => data.platform.includes('atmelavr')
-    },
-    {
-      name: 'Set Fuses',
-      args: ['run', '--target', 'fuses'],
-      multienv: true,
-      filter: data => data.platform.includes('atmelavr')
-    },
-    {
-      name: 'Upload and Set Fuses',
-      args: ['run', '--target', 'fuses', '--target', 'upload'],
-      multienv: true,
-      filter: data => data.platform.includes('atmelavr')
-    },
-    {
-      name: 'Upload using Programmer and Set Fuses',
-      args: ['run', '--target', 'fuses', '--target', 'program'],
-      multienv: true,
-      filter: data => data.platform.includes('atmelavr')
-    },
-    {
-      name: 'Upload File System image',
-      args: ['run', '--target', 'uploadfs'],
-      multienv: true,
-      filter: data =>
-        data.platform.includes('espressif') || data.platform.includes('riscv_gap')
-    },
-    {
-      name: 'Erase Flash',
-      args: ['run', '--target', 'erase'],
-      multienv: true,
-      filter: data =>
-        data.platform.includes('espressif') || data.platform.includes('nordicnrf')
+      multienv: true
     },
     {
       name: 'Devices',
       args: ['device', 'list']
-    },
-    {
-      name: 'Test',
-      args: ['test'],
-      multienv: true
-    },
-    {
-      name: 'Check',
-      args: ['check'],
-      multienv: true
-    },
-    {
-      name: 'Pre-Debug',
-      description: 'build in debug mode',
-      args: ['debug'],
-      multienv: true
     },
     {
       name: 'Clean',
@@ -96,31 +41,56 @@ export class ProjectTasks {
       multienv: true
     },
     {
+      name: 'Test',
+      args: ['test'],
+      group: 'Advanced',
+      multienv: true
+    },
+    {
+      name: 'Check',
+      args: ['check'],
+      group: 'Advanced',
+      multienv: true
+    },
+    {
+      name: 'Pre-Debug',
+      description: 'build in debug mode',
+      args: ['debug'],
+      group: 'Advanced',
+      multienv: true
+    },
+    {
       name: 'Verbose Build',
       args: ['run', '--verbose'],
+      group: 'Advanced',
       multienv: true
     },
     {
       name: 'Verbose Upload',
       args: ['run', '--verbose', '--target', 'upload'],
+      group: 'Advanced',
       multienv: true
     },
     {
       name: 'Remote Upload',
       args: ['remote', 'run', '--target', 'upload'],
+      group: 'PIO Remote',
       multienv: true
     },
     {
       name: 'Remote Monitor',
-      args: ['remote', 'device', 'monitor']
+      args: ['remote', 'device', 'monitor'],
+      group: 'PIO Remote'
     },
     {
       name: 'Remote Devices',
-      args: ['remote', 'device', 'list']
+      args: ['remote', 'device', 'list'],
+      group: 'PIO Remote'
     },
     {
       name: 'Remote Test',
       args: ['remote', 'test'],
+      group: 'PIO Remote',
       multienv: true
     }
   ];
@@ -130,76 +100,101 @@ export class ProjectTasks {
     this.ide = ide;
   }
 
-  async getTasks() {
-    if (!this.projectDir) {
-      return [];
-    }
-    const projectEnvs = [];
+  async getGenericTasks() {
+    // Generic tasks
+    const result = ProjectTasks.genericTasks.map(
+      task => new TaskItem(task.name, task.description, task.args.slice(0), task.group)
+    );
+    // Miscellaneous tasks
+    result.push(
+      new TaskItem(
+        'Rebuild IntelliSense Index',
+        undefined,
+        ['init', '--ide', this.ide],
+        'Miscellaneous'
+      ),
+      new TaskItem(
+        'Update project libraries',
+        undefined,
+        ['lib', 'update'],
+        'Miscellaneous'
+      ),
+      new TaskItem(
+        'Update All (libraries, dev-platforms, and packages)',
+        undefined,
+        ['update'],
+        'Miscellaneous'
+      ),
+      new TaskItem('Upgrade PlatformIO Core', undefined, ['upgrade'], 'Miscellaneous')
+    );
+    return result;
+  }
 
-    const prevCWD = process.cwd();
-    process.chdir(this.projectDir);
+  async fetchEnvTasks(name) {
+    const result = [];
+    const usedTitles = [];
+    for (const task of ProjectTasks.genericTasks) {
+      if (!task.multienv) {
+        continue;
+      }
+      usedTitles.push(task.name);
+      result.push(
+        new TaskItem(
+          task.name,
+          task.description,
+          [...task.args.slice(0), '--environment', name],
+          task.group
+        )
+      );
+    }
+    // dev-platform targets
     try {
-      const config = new ProjectConfig(path.join(this.projectDir, 'platformio.ini'));
-      for (const env of config.envs()) {
-        const platform = config.get(`env:${env}`, 'platform');
-        if (!platform) {
+      for (const target of await this.fetchEnvTargets(name)) {
+        if (usedTitles.includes(target.title)) {
           continue;
         }
-        projectEnvs.push({
-          env,
-          platform
-        });
+        result.push(
+          new TaskItem(
+            target.title || target.name,
+            target.description,
+            ['run', '--target', target.name, '--environment', name],
+            target.scope === 'system' ? 'Platform' : 'Custom'
+          )
+        );
       }
     } catch (err) {
-      console.warn(
-        `Could not parse "platformio.ini" file in ${this.projectDir}: ${err}`
+      console.error(
+        `Could not fetch project targets for '${name}' environment => ${err}`
       );
-      return [];
     }
-    // restore original CWD
-    process.chdir(prevCWD);
-
-    const result = [];
-
-    // base tasks
-    ProjectTasks.baseTasks.forEach(task => {
-      if (!task.filter || projectEnvs.some(data => task.filter(data))) {
-        result.push(new TaskItem(task.name, task.description, task.args.slice(0)));
-      }
-    });
-
-    // multi environment tasks
-    if (projectEnvs.length > 1) {
-      projectEnvs.forEach(data => {
-        ProjectTasks.baseTasks.forEach(task => {
-          if (task.multienv && (!task.filter || task.filter(data))) {
-            result.push(
-              new TaskItem(task.name, task.description, [
-                ...task.args.slice(0),
-                '--environment',
-                data.env
-              ])
-            );
-          }
-        });
-      });
-    }
-
-    // Misc tasks
-    result.push(new TaskItem('Update project libraries', undefined, ['lib', 'update']));
-    result.push(
-      new TaskItem('Rebuild IntelliSense Index', undefined, ['init', '--ide', this.ide])
-    );
-
     return result;
+  }
+
+  async fetchEnvTargets(name) {
+    const scriptLines = [
+      'import json, os',
+      'from platformio.project.helpers import load_project_ide_data',
+      `print(json.dumps(load_project_ide_data(os.getcwd(), '${name}')["targets"]))`
+    ];
+    const output = await proc.getCommandOutput(
+      await core.getCorePythonExe(),
+      ['-c', scriptLines.join(';')],
+      {
+        spawnOptions: {
+          cwd: this.projectDir
+        }
+      }
+    );
+    return JSON.parse(output.trim());
   }
 }
 
 export class TaskItem {
-  constructor(name, description, args) {
+  constructor(name, description, args, group = 'Generic') {
     this.name = name;
     this.description = description;
     this.args = args;
+    this.group = group;
   }
 
   get coreTarget() {
